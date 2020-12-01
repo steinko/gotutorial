0377982c0b3a8539f1f95a92b92eb76789b69670
stein korsveien <stein_korsveien@msn.com>
2020-12-01 06:46:54 +0100

    Refactord

--- hello/hello.go
+++ hello/hello.go
@@ -1,14 +1,16 @@
 package main
 
 const englishPrefix = "Hello "
+const spanishPrefix = "Hola "
+const spanish = "Spanish"
 
 func Hello(name string , language string) string {
      if name == "" {
         name = "World"
       }
       
-      if language == "Spanish" { 
-          return "Hola " + name
+      if language == spanish { 
+          return spanishPrefix + name
       }
       
      return englishPrefix + name
